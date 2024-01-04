278873e943e66956cb102fe02f9497623b1f35ab
HangNT169 <104345338+HangNT169@users.noreply.github.com>
2024-01-04 16:58:29 +0700

    Buổi 2: Kiểu dữ liệu

---
+++ Buoi2_KieuDuLieu.c
@@ -0,0 +1,34 @@
+#include<stdio.h> // Khai bao thu vien 
+int main(){
+    // Code o giua 
+    // Ket thuc moi dong lenh la dau cham phay ; 
+    // De in ra man hinh: printf
+    // \n: Xuong dong 
+    // \t: Lui vao 1 tab 
+    // printf("Xin chao toi la abc\nasdfhkadsfhkasdhjfhk");
+    // printf("\n\2hsg");
+    // printf("\n\3hsg");
+    printf("\n\4gaf");  
+    // In thong tin cua nyc bao gom : ten , tuoi, dia chi ra man hinh 
+    // Cac kieu du lieu trong lap trinh:
+    // so nguyen: 1,-1.. => int/long 
+    // so thuc: 1.1.... => float/double 
+    // chuoi/ky tu : char 
+    // Dung sai => bool 
+    // Cach khai bao bien 
+    // kieu du lieu ten bien;
+    // VD: Khai bao bien so nguyen 
+    int a;
+    // Khai bao bien so thuc 
+    float b;
+    // Khoi tao gia tri cua so nguyen la 5 
+    int c = 5;
+    // Khoi tao gia tri cua so thuc la 5.5
+    // Khoi tao gia tri cua so nguyen la 3 
+    // Khoi tao gia tri cua so thuc la 9.9
+    float d = 5.5;
+    // Quy tac dat ten :
+    // camelcase (Lac da): numberFirstSecond 
+    // snakecase(ran): number_first
+    return 0;
+}
